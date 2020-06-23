da0c66fa5f4f2ce336c462ae1d14e2f882f78ec6
Alex Hapgood <ach1@vt.edu>
2020-06-23 01:48:59 -0400

    stack soln.

--- stack.js
+++ stack.js
@@ -40,19 +40,118 @@ class Stack {
     }
 
     // display
+    display() {
+        let currNode = this.peek();
+        const res = [];
+        while (currNode !== null && !this.isEmpty()) {
+            //console.log('node: ', currNode)
+            res.push(currNode);
+            currNode = currNode.next;
+        }
+        return res;
+    }
+
+    deStack(data) {
+        let currNode = this.peek();
+        let prevNode = this.peek();
+
+        if (this.peek().data === data) this.pop();
+        else {
+            while (currNode !== null && !this.isEmpty()) {
+                if (currNode.data === data) {
+                    prevNode.next = currNode.next;
+                    currNode = currNode.next;
+                } else {
+                    prevNode = currNode;
+                    currNode = currNode.next;
+                }
+            }
+            return -1;
+        }
+        return 1;
+    }
+
 }
 
 function main() {
     const FILO = new Stack();
-    FILO.push('kirk');
-    FILO.push('spock');
-    FILO.push('mccoy');
-    FILO.push('scotty')
+    // FILO.push('kirk');
+    // FILO.push('spock');
+    // FILO.push('mccoy');
+    // FILO.push('scotty')
 
-    FILO.pop();
+    FILO.push(3);
+    FILO.push(8);
+    FILO.push(7);
+    FILO.push(23);
+    FILO.push(4);
 
-    console.log(FILO.peek(), ', ', FILO.isEmpty())
-    //console.log(JSON.stringify(FILO, null, 2));
+    console.log(JSON.stringify(sortStack(FILO).display(), null, 2));
+
+    // FILO.deStack('mccoy');
+    // console.log(JSON.stringify(FILO.display(), null, 2));
+    // console.log(FILO.peek(), ', ', FILO.isEmpty())
+    // console.log(JSON.stringify(FILO, null, 2));
+
 }
 
 main();
+
+
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    const stackStr = []
+    const PAL = new Stack();
+
+    s.split('').forEach(letter => {
+        PAL.push(letter);
+    })
+
+    PAL.display().forEach(node => {
+        stackStr.push(node.data);
+    })
+
+
+    return stackStr.join('') === s;
+
+}
+
+// True, true, true, false
+// console.log(is_palindrome("dad"));
+// console.log(is_palindrome("A man, a plan, a canal: Panama"));
+// console.log(is_palindrome("1001"));
+// console.log(is_palindrome("Tauhida"));
+
+function checkParentheses(input) {
+    let PAR = new Stack();
+    if (input[0] === ')' || input[input.length - 1] === '(') {
+        return false
+    }
+    input.split('').forEach(ltr => {
+        if (ltr === '(') {
+            PAR.push(ltr)
+        }
+        else if (ltr === ')') {
+            PAR.pop()
+        }
+    })
+    return PAR.top === null;
+}
+
+// console.log(checkParentheses('(((((((((((((())))))))))))))'))
+// console.log(checkParentheses('()(())()()((())())'))
+// console.log(checkParentheses('(((()))'))
+
+function sortStack(stack) {
+    let asc = new Stack();
+    while (!stack.isEmpty()) {
+        let currNode = stack.top;
+        stack.pop()
+        while (!asc.isEmpty() && asc.top.data < currNode.data) {
+            stack.push(asc.top.data)
+            asc.pop()
+        }
+        asc.push(currNode.data)
+    }
+    return asc
+}
