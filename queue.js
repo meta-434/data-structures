3ff6c80d6c86a3a192a65584f45ce9e927e009ad
Alex Hapgood <ach1@vt.edu>
2020-06-23 02:25:11 -0400

    queue wip

--- queue.js
+++ queue.js
@@ -13,6 +13,73 @@ class Queue {
         this.last = null;
     }
     // enqueue
+    enqueue(data) {
+        const node = new _Node(data);
 
+        if (this.first === null) {
+            this.first = node;
+        }
+
+        if (this.last) {
+            this.last.next = node;
+        }
+
+        this.last = node
+    }
     // dequeue
+    dequeue() {
+        if (this.first === null) {
+            return;
+        }
+
+        const node = this.first;
+        this.first = this.first.next;
+
+        if (node === this.last) {
+            this.last = null;
+        }
+
+        return node.value
+    }
+
+    peek() {
+        if(this.first !== null) {
+            return this.first.data;
+        }
+        return 'queue is empty'
+    }
+
+    isEmpty() {
+        return this.first == null;
+    }
 }
+
+function main(){
+    const QUEUE = new Queue();
+
+    QUEUE.enqueue('hi');
+    QUEUE.enqueue(',');
+    QUEUE.enqueue('how');
+    QUEUE.enqueue('are');
+    QUEUE.enqueue('ya');
+    QUEUE.enqueue('?');
+
+    console.log(JSON.stringify(QUEUE, null, 2));
+}
+
+// main();
+
+function mainQ() {
+    const starTrekQ = new Queue();
+    starTrekQ.enqueue("Kirk");
+    starTrekQ.enqueue("Spock");
+    starTrekQ.enqueue("Uhura");
+    starTrekQ.enqueue("Sulu");
+    starTrekQ.enqueue("Checkov");
+    starTrekQ.dequeue();
+    starTrekQ.dequeue();
+    starTrekQ.enqueue("Kirk");
+    console.log(JSON.stringify(starTrekQ, null, 2))
+}
+
+mainQ();
